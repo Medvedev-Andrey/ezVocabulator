64ffb8514259de9aadae45483b5de1afd72f0260
Andrey <super.user.maa@gmail.com>
2021-12-25 15:26:33 +0300

    Implemented simple Telegram bot. Uses system environment variable to connect to Telegram API and replies at each message with similar text

--- main.go
+++ main.go
@@ -1,4 +1,33 @@
 package main
 
+import (
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
 func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_API_TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 30
+
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+		msg.Text = update.Message.Text + " ... reply!"
+
+		if _, err := bot.Send(msg); err != nil {
+			panic(err)
+		}
+	}
 }
