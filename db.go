46e9050006a2f0cf749a39a23fecbcef6ed308d8
Andrey <super.user.maa@gmail.com>
2022-01-04 20:50:10 +0300

    Changed Dictionary requests DB scheme

--- db.go
+++ db.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/binary"
 	"fmt"
 	"log"
 	"regexp"
@@ -10,7 +11,7 @@ import (
 )
 
 type dictRequestsRow struct {
-	date   string
+	date   time.Time
 	userID int
 	data   string
 }
@@ -21,7 +22,7 @@ func ensureDictionaryRequestDBExists(db *sql.DB) error {
 	createTableStatement := `
 		CREATE TABLE IF NOT EXISTS dict_requests 
 		( 
-			date char(10) NOT NULL, 
+			date timestamp NOT NULL, 
 			user_id int NOT NULL, 
 			data text, 
 			PRIMARY KEY (date, user_id)
@@ -33,7 +34,7 @@ func ensureDictionaryRequestDBExists(db *sql.DB) error {
 
 func storeDictionaryRequest(db *sql.DB, userID int, item string) error {
 	daysCount := 1
-	date := time.Now().AddDate(0, 0, daysCount).Format("2006-01-02")
+	date := getCurrentDateTimestamp().AddDate(0, 0, daysCount)
 
 	var err error
 	defer func() {
@@ -163,10 +164,82 @@ func getUserRequests(db *sql.DB, userID int) ([]string, error) {
 	return userRequests, nil
 }
 
+func getCurrentDateTimestamp() time.Time {
+	currentDate := time.Now()
+	return time.Date(currentDate.Year(), currentDate.Month(), currentDate.Day(), 0, 0, 0, 0, currentDate.Location())
+}
+
 func formatDictionaryRequest(item string, daysCount int) string {
 	return fmt.Sprintf("^%s,%d", item, daysCount)
 }
 
 func containsDuplicate(data string, item string) bool {
 	return strings.Contains(data, fmt.Sprintf("^%s,", item))
+}
+
+type userDictionaryRequest struct {
+	item             string
+	daysToTrainAfter int
+}
+
+func getUserRequestsToTrain(db *sql.DB, userID int, count int) ([]userDictionaryRequest, error) {
+	var err error
+	defer func() {
+		if err != nil {
+			log.Printf("Failed requesting words to train for user with ID %d. %s", userID, err)
+		} else {
+			log.Printf("Successfully requested words to train for user with ID %d", userID)
+		}
+	}()
+
+	date := getCurrentDateTimestamp()
+
+	log.Printf("Requesting words to train for user with ID %d by %s ...", userID, date)
+	getUserDataStatement := `
+		SELECT data FROM dict_requests 
+		WHERE user_id = $1 AND date <= $2
+		ORDER BY date`
+	var rows *sql.Rows
+	rows, err = db.Query(getUserDataStatement, userID, date)
+	if err != nil && err != sql.ErrNoRows {
+		return nil, err
+	}
+
+	defer rows.Close()
+	var userDictionaryRequests []userDictionaryRequest
+
+	if err == sql.ErrNoRows {
+		log.Printf("Found no words to train for user with ID %d", userID)
+		err = nil
+		return userDictionaryRequests, nil
+	}
+
+	re := regexp.MustCompile(`\^([^\^]*),([0-9]+)`)
+	for rows.Next() && len(userDictionaryRequests) < count {
+		var data []byte
+		err = rows.Scan(&data)
+
+		if err != nil {
+			log.Printf("Error while acquiring user requests history from row. %q", err)
+			continue
+		} else if len(data) == 0 {
+			log.Printf("Empty user requests history for %d", userID)
+			continue
+		}
+
+		for _, match := range re.FindAllSubmatch(data, -1) {
+			if len(userDictionaryRequests) >= count {
+				break
+			}
+
+			newRequest := userDictionaryRequest{
+				item:             string(match[1]),
+				daysToTrainAfter: int(binary.BigEndian.Uint32(match[2])),
+			}
+
+			userDictionaryRequests = append(userDictionaryRequests, newRequest)
+		}
+	}
+
+	return userDictionaryRequests, nil
 }
